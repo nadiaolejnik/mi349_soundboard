f7472ba020c9a83aa42394aeadad67451431255b
Nadia Olejnik <olejnikn@msu.edu>
2022-11-17 16:15:29 -0500

    changes

--- soundboard.js
+++ soundboard.js
@@ -1,1 +1,30 @@
+var firstButton = document.getElementById('first-button')
 
+firstButton.addEventListener('click', function () {
+    console.log('I was clicked')
+})
+firstButton.addEventListener('mouseenter', function () {
+    console.log('I was clicked')
+})
+
+
+
+var secondButton = document.getElementById('second-button')
+
+secondButton.addEventListener('click', function () {
+    console.log('I was clicked')
+})
+secondButton.addEventListener('mouseenter', function () {
+    console.log('I was clicked')
+})
+
+
+
+var thirdButton = document.getElementById('third-button')
+
+thirdButton.addEventListener('click', function () {
+    console.log('I was clicked')
+})
+thirdButton.addEventListener('mouseenter', function () {
+    console.log('I was clicked')
+})
